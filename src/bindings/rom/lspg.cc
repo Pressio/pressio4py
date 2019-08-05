1daffb3b70eb3de4b533f4c2ede831f861b2408e
Francesco Rizzi <fnrizzi@sandia.gov>
2019-08-05 18:28:39 +0200

    adding Galerkin

--- src/bindings/rom/lspg.cc
+++ src/bindings/rom/lspg.cc
@@ -1,7 +1,6 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/functional.h>
-//#include <pybind11/eigen.h>
 #include <pybind11/numpy.h>
 
 #include "UTILS_ALL"
@@ -10,48 +9,65 @@
 #include "ODE_ALL"
 #include "ROM_BASIC"
 #include "ROM_LSPG"
+#include "ROM_GALERKIN"
 
-  // pybind11::enum_<::pressio::ode::ImplicitEnum>(m, "ImplicitEnum", pybind11::arithmetic())
-  //   .value("Undefined", ::pressio::ode::ImplicitEnum::Undefined)
-  //   .value("Euler",	::pressio::ode::ImplicitEnum::Euler)
-  //   .value("BDF2",	::pressio::ode::ImplicitEnum::BDF2);
+struct MyTypes{
+  using scalar_t	= double;
+  using py_arr		= pybind11::array_t<scalar_t, pybind11::array::c_style>;
+  using fom_t		= pybind11::object;
+  using ops_t		= pybind11::object;
+  using rom_state_t	= py_arr;
+  using step_t		= int;
+};
 
-PYBIND11_MODULE(pressio4py, m) {
-
-  // type aliases
-  using scalar_t = double;
-  using py_arr = pybind11::array_t<scalar_t, pybind11::array::c_style>;
-  using fom_t = pybind11::object;
-  using ops_t = pybind11::object;
-  using step_t = int;
-  using lspg_state_t	= py_arr;
-
-  // linear decoder
-  using decoder_jac_t	= py_arr;
+template <typename types>
+struct LinearDecoderBinder{
+  using ops_t		= typename types::ops_t;
+  using decoder_jac_t	= typename types::py_arr;
   using decoder_t	= ::pressio::rom::PyLinearDecoder<decoder_jac_t, ops_t>;
 
-  pybind11::class_<decoder_t>(m, "LinearDecoder")
+  static void doBind(pybind11::module & m){
+    pybind11::class_<decoder_t>(m, "LinearDecoder")
       .def(pybind11::init< const decoder_jac_t &, pybind11::object &>());
+  }
+};
+
+PYBIND11_MODULE(pressio4py, m) {
+  using mytypes = MyTypes;
+  LinearDecoderBinder<mytypes>::doBind(m);
+}
+
+PYBIND11_MODULE(pressio4pyLSPG, m) {
+  using mytypes = MyTypes;
+
+  using scalar_t	= typename mytypes::scalar_t;
+  using py_arr		= typename mytypes::py_arr;
+  using fom_t		= typename mytypes::fom_t;
+  using ops_t		= typename mytypes::ops_t;
+  using rom_state_t	= typename mytypes::rom_state_t;
+  using step_t		= typename mytypes::step_t;
+
+  using decoderbind_t = LinearDecoderBinder<mytypes>;
+  using decoder_t = typename decoderbind_t::decoder_t;
 
   // lspg problem
-  // TODO: need to figure out how to handle enums from python
   constexpr auto ode_case  = ::pressio::ode::ImplicitEnum::Euler;
-
   using lspg_problem_type = pressio::rom::DefaultLSPGTypeGenerator<
-    fom_t, ode_case, decoder_t, lspg_state_t, ops_t>;
+    fom_t, ode_case, decoder_t, rom_state_t, ops_t>;
   using lspg_prob_gen	= pressio::rom::LSPGUnsteadyProblemGenerator<lspg_problem_type>;
+
   using lspg_stepper_t	= typename lspg_prob_gen::lspg_stepper_t;
   using res_pol_t	= typename lspg_prob_gen::lspg_residual_policy_t;
   using jac_pol_t	= typename lspg_prob_gen::lspg_jacobian_policy_t;
 
   // concrete LSPG stepper
   pybind11::class_<lspg_stepper_t>(m, "LspgStepperEuler")
-    .def(pybind11::init<const py_arr &, const fom_t &,
+    .def(pybind11::init<const rom_state_t &, const fom_t &,
 			const res_pol_t &, const jac_pol_t &>());
 
   pybind11::class_<lspg_prob_gen>(m, "LspgProblemEuler")
     .def(pybind11::init<const fom_t &, const py_arr &, decoder_t &,
-			lspg_state_t &, scalar_t, const ops_t &>())
+			rom_state_t &, scalar_t, const ops_t &>())
     .def("getStepper", &lspg_prob_gen::getStepperRef);
 
   // linear solver type: use pybind::object because we use numpy solver
@@ -77,16 +93,148 @@ PYBIND11_MODULE(pressio4py, m) {
     .def("setTolerance", &iter_base_t::setTolerance);
 
   pybind11::class_<nonlin_solver_t, iter_base_t, nls_base_t>(m, "GaussNewton")
-    .def(pybind11::init<const lspg_stepper_t &, const py_arr &,
-			linear_solver_t &, pybind11::object &>());
+    .def(pybind11::init<const lspg_stepper_t &, const rom_state_t &,
+			linear_solver_t &, ops_t &>());
 
   // integrator
   m.def("integrateNSteps",
   	&::pressio::ode::integrateNSteps<
-  	lspg_stepper_t, py_arr, scalar_t, step_t, nonlin_solver_t>,
+  	lspg_stepper_t, rom_state_t, scalar_t, step_t, nonlin_solver_t>,
   	"Integrate N Steps");
 }
 
+
+PYBIND11_MODULE(pressio4pyGalerkin, m) {
+  using mytypes = MyTypes;
+
+  using scalar_t	= typename mytypes::scalar_t;
+  using py_arr		= typename mytypes::py_arr;
+  using fom_t		= typename mytypes::fom_t;
+  using ops_t		= typename mytypes::ops_t;
+  using rom_state_t	= typename mytypes::rom_state_t;
+  using step_t		= typename mytypes::step_t;
+
+  using decoderbind_t = LinearDecoderBinder<mytypes>;
+  using decoder_t = typename decoderbind_t::decoder_t;
+
+  // RK4 Galerkin problem
+  constexpr auto ode_case  = ::pressio::ode::ExplicitEnum::RungeKutta4;
+  using galerkin_problem_t = ::pressio::rom::DefaultGalerkinExplicitTypeGenerator<
+    ode_case, rom_state_t, fom_t, decoder_t>;
+
+  using galerkin_problem_gen = pressio::rom::GalerkinProblemGenerator<galerkin_problem_t>;
+
+  using galerkin_stepper_t  = typename galerkin_problem_gen::galerkin_stepper_t;
+  using res_pol_t	    = typename galerkin_problem_gen::galerkin_residual_policy_t;
+
+  // stepper
+  pybind11::class_<galerkin_stepper_t>(m, "GalerkinStepperRK4")
+    .def(pybind11::init<const rom_state_t &, const fom_t &, const res_pol_t &>());
+
+  pybind11::class_<galerkin_problem_gen>(m, "GalerkinProblemRK4")
+    .def(pybind11::init<const fom_t &, const py_arr &, decoder_t &,
+  			rom_state_t &, scalar_t, const ops_t &>())
+    .def("getStepper", &galerkin_problem_gen::getStepperRef);
+
+  // integrator
+  m.def("integrateNSteps",
+  	&::pressio::ode::integrateNSteps<
+  	galerkin_stepper_t, rom_state_t, scalar_t, step_t>,
+  	"Integrate N Steps");
+}
+
+
+
+
+
+
+
+
+
+
+
+// PYBIND11_MODULE(pressio4py, m) {
+
+//   // type aliases
+//   using scalar_t	= double;
+//   using py_arr		= pybind11::array_t<scalar_t, pybind11::array::c_style>;
+//   using fom_t		= pybind11::object;
+//   using ops_t		= pybind11::object;
+//   using step_t		= int;
+//   using lspg_state_t	= py_arr;
+
+//   // linear decoder
+//   using decoder_jac_t	= py_arr;
+//   using decoder_t	= ::pressio::rom::PyLinearDecoder<decoder_jac_t, ops_t>;
+
+//   pybind11::class_<decoder_t>(m, "LinearDecoder")
+//       .def(pybind11::init< const decoder_jac_t &, pybind11::object &>());
+
+//   // lspg problem
+//   // TODO: need to figure out how to handle enums from python
+//   constexpr auto ode_case  = ::pressio::ode::ImplicitEnum::Euler;
+
+//   using lspg_problem_type = pressio::rom::DefaultLSPGTypeGenerator<
+//     fom_t, ode_case, decoder_t, lspg_state_t, ops_t>;
+//   using lspg_prob_gen	= pressio::rom::LSPGUnsteadyProblemGenerator<lspg_problem_type>;
+//   using lspg_stepper_t	= typename lspg_prob_gen::lspg_stepper_t;
+//   using res_pol_t	= typename lspg_prob_gen::lspg_residual_policy_t;
+//   using jac_pol_t	= typename lspg_prob_gen::lspg_jacobian_policy_t;
+
+//   // concrete LSPG stepper
+//   pybind11::class_<lspg_stepper_t>(m, "LspgStepperEuler")
+//     .def(pybind11::init<const py_arr &, const fom_t &,
+// 			const res_pol_t &, const jac_pol_t &>());
+
+//   pybind11::class_<lspg_prob_gen>(m, "LspgProblemEuler")
+//     .def(pybind11::init<const fom_t &, const py_arr &, decoder_t &,
+// 			lspg_state_t &, scalar_t, const ops_t &>())
+//     .def("getStepper", &lspg_prob_gen::getStepperRef);
+
+//   // linear solver type: use pybind::object because we use numpy solver
+//   using linear_solver_t = pybind11::object;
+
+//   // non-linear solver type
+//   using hessian_t = py_arr; // hessian is a numpy array
+//   using nonlin_solver_t = ::pressio::solvers::iterative::PyGaussNewton
+//     <lspg_stepper_t, py_arr, py_arr, py_arr,
+//      hessian_t, linear_solver_t, scalar_t>;
+
+//   // base types
+//   using nls_base_t = ::pressio::solvers::NonLinearSolverBase<nonlin_solver_t>;
+//   using iter_base_t = ::pressio::solvers::IterativeBase<scalar_t>;
+
+//   pybind11::class_<nls_base_t>(m, "NonLinSolverBase")
+//     .def("solve", &nls_base_t::template solve<lspg_stepper_t, py_arr>);
+
+//   pybind11::class_<iter_base_t>(m, "IterBase")
+//     .def("getMaxIterations", &iter_base_t::getMaxIterations)
+//     .def("setMaxIterations", &iter_base_t::setMaxIterations)
+//     .def("getTolerance", &iter_base_t::getTolerance)
+//     .def("setTolerance", &iter_base_t::setTolerance);
+
+//   pybind11::class_<nonlin_solver_t, iter_base_t, nls_base_t>(m, "GaussNewton")
+//     .def(pybind11::init<const lspg_stepper_t &, const py_arr &,
+// 			linear_solver_t &, pybind11::object &>());
+
+//   // integrator
+//   m.def("integrateNSteps",
+//   	&::pressio::ode::integrateNSteps<
+//   	lspg_stepper_t, py_arr, scalar_t, step_t, nonlin_solver_t>,
+//   	"Integrate N Steps");
+// }
+
+
+
+
+
+
+
+
+  // pybind11::enum_<::pressio::ode::ImplicitEnum>(m, "ImplicitEnum", pybind11::arithmetic())
+  //   .value("Undefined", ::pressio::ode::ImplicitEnum::Undefined)
+  //   .value("Euler",	::pressio::ode::ImplicitEnum::Euler)
+  //   .value("BDF2",	::pressio::ode::ImplicitEnum::BDF2);
 
 
 
