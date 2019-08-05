7a53e5b7d05a72334f6ad55ab405d998aa435f24
Francesco Rizzi <fnrizzi@sandia.gov>
2019-08-05 22:00:21 +0200

    updating

--- src/bindings/rom/linear_decoder.cc
+++ src/bindings/rom/linear_decoder.cc
@@ -9,15 +9,6 @@
 #include "ROM_BASIC"
 #include "types.hpp"
 
-// struct MyTypes{
-//   using scalar_t	= double;
-//   using py_arr		= pybind11::array_t<scalar_t, pybind11::array::c_style>;
-//   using fom_t		= pybind11::object;
-//   using ops_t		= pybind11::object;
-//   using rom_state_t	= py_arr;
-//   using step_t		= int;
-// };
-
 template <typename types>
 struct LinearDecoderBinder{
   using ops_t		= typename types::ops_t;
