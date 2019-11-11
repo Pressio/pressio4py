08d0cd7361f977dbf0a576e738de8afb7ef029f1
Francesco Rizzi <fnrizzi@sandia.gov>
2019-11-11 13:35:27 +0100

    remove eigen for now

--- sampleBuildScripts/build.sh
+++ sampleBuildScripts/build.sh
@@ -5,7 +5,6 @@ source ${envScript}
 
 WORKPATH="$HOME/Desktop"
 TPLSPATH="${WORKPATH}/clang/clang700_ompi400_debug_shared"
-EIGENPATH="${TPLSPATH}/eigen/install/include/eigen3"
 PYBIND11PATH="${TPLSPATH}/pybind11/install"
 PRESSIOPATH="${WORKPATH}/clang/clang700_ompi400_debug_shared/pressio/install/include"
 
@@ -16,7 +15,6 @@ bdirname=build
 rm -rf ${bdirname} && mkdir ${bdirname} && cd ${bdirname}
 cmake -DCMAKE_CXX_COMPILER=${CC} \
       -DCMAKE_CXX_COMPILER=${CXX} \
-      -DEIGEN_INCLUDE_DIR=${EIGENPATH} \
       -DPYBIND11_DIR=${PYBIND11PATH} \
       -DPRESSIO_INCLUDE_DIR=${PRESSIOPATH} \
       -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=${PFX} \
