b7fba45fb685ef6170c7d1d5da741bef14bac78e
Francesco Rizzi <fnrizzi@sandia.gov>
2021-01-28 11:24:42 +0100

    update website

--- docs/conf.py
+++ docs/conf.py
@@ -54,9 +54,7 @@ LINKS_NAVBAR2 = [
   ('Namespaces', 'namespaces', [])
 ]
 
-PLUGINS = ['m.htmlsanity', 'm.math',
-           'm.code', 'm.components',
-           'm.dot', 'm.images']
+PLUGINS = ['m.htmlsanity', 'm.math', 'm.code', 'm.components', 'm.dot', 'm.images']
 
 SHOW_UNDOCUMENTED = "YES"
 
