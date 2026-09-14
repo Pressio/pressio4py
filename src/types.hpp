980dfcad8c4aada81272b6905a1efcdb4fda681e
eparish1 <eric.parish9@gmail.com>
2026-09-14 15:31:18 -0500

    Adapt nonlinear system wrapper to Pressio 0.17 fused API

--- src/types.hpp
+++ src/types.hpp
@@ -3,45 +3,11 @@
 // ************************************************************************
 //
 // types.hpp
-//                     		  Pressio
+//                         Pressio
 //                         Copyright 2019
 //    National Technology & Engineering Solutions of Sandia, LLC (NTESS)
 //
-// Under the terms of Contract DE-NA0003525 with NTESS, the
-// U.S. Government retains certain rights in this software.
-//
-// Pressio is licensed under BSD-3-Clause terms of use:
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-//
-// 1. Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the copyright holder nor the names of its
-// contributors may be used to endorse or promote products derived
-// from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-//
-// Questions? Contact Francesco Rizzi (fnrizzi@sandia.gov)
-//
+// Pressio is licensed under BSD-3-Clause terms of use.
 // ************************************************************************
 //@HEADER
 */
@@ -50,6 +16,7 @@
 #define PRESSIO4PY_PYBINDINGS_TYPES_HPP_
 
 #include <cstdint>
+#include <optional>
 #include <pybind11/pybind11.h>
 #include <pybind11/functional.h>
 #include <pybind11/numpy.h>
@@ -58,9 +25,9 @@
 #include <pybind11/operators.h>
 
 namespace pressio4py{
-using scalar_t	= double;
-using py_c_arr	= pybind11::array_t<scalar_t, pybind11::array::c_style>;
-using py_f_arr	= pybind11::array_t<scalar_t, pybind11::array::f_style>;
+using scalar_t = double;
+using py_c_arr = pybind11::array_t<scalar_t, pybind11::array::c_style>;
+using py_f_arr = pybind11::array_t<scalar_t, pybind11::array::f_style>;
 }//end namespace pressio4py
 
 #include "pressio/type_traits.hpp"
@@ -99,30 +66,42 @@ using ode_observer_wrapper_type  = ::pressio4py::OdeCollectorWrapper<void>;
 using ode_dt_setter_wrapper_type = ::pressio4py::OdeTimeStepSizeSetterWrapper<scalar_t>;
 
 
+// Compatibility adapter from the historical pressio4py residual/Jacobian API
+// to the fused nonlinear-system concept used by pressio-rom 0.17.
 template<class ScalarType, class StateType, class ResidualType, class JacobianType>
 class ResJacInterface{
-
 protected:
   pybind11::object pyObj_;
+  StateType statePrototype_;
 
 public:
-  using scalar_type       = ScalarType;
-  using state_type	  = StateType;
-  using residual_type	  = ResidualType;
-  using jacobian_type	  = JacobianType;
+  using scalar_type = ScalarType;
+  using state_type = StateType;
+  using residual_type = ResidualType;
+  using jacobian_type = JacobianType;
 
 public:
-  explicit ResJacInterface(pybind11::object pyObj)
-    : pyObj_(pyObj){}
+  ResJacInterface(pybind11::object pyObj, const StateType & statePrototype)
+    : pyObj_(pyObj), statePrototype_(statePrototype){}
 
   ResJacInterface() = delete;
   ~ResJacInterface() = default;
   ResJacInterface(const ResJacInterface&) = default;
   ResJacInterface & operator=(const ResJacInterface &) = default;
-  // note, we don't declare move constructors because pybind11::object
-  // gives troubles so just use copy
 
 public:
+  state_type createState() const{
+    // Newer Pressio systems expose createState().  Legacy pressio4py user
+    // systems generally do not, so use the state supplied to the Python
+    // create_*_solver call as the shape/layout prototype.
+    if (pybind11::hasattr(pyObj_, "createState")){
+      return pyObj_.attr("createState")();
+    }
+    return pybind11::module_::import("numpy")
+      .attr("zeros_like")(statePrototype_)
+      .template cast<state_type>();
+  }
+
   residual_type createResidual() const{
     return pyObj_.attr("createResidual")();
   }
@@ -137,6 +116,16 @@ public:
 
   void jacobian(const state_type & state, jacobian_type & jac) const{
     pyObj_.attr("jacobian")(state, jac);
+  }
+
+  void residualAndJacobian(const state_type & state,
+                           residual_type & R,
+                           std::optional<jacobian_type*> jac) const
+  {
+    residual(state, R);
+    if (jac){
+      this->jacobian(state, **jac);
+    }
   }
 };
 
